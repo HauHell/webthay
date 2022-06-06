9bd93b8e7baf0d6a781cf1efc9dd5983ea424f42
HauHell <20662002@kthcm.edu.vn>
2022-06-06 11:14:13 +0700

    Update blog_categories.php

--- blog_categories.php
+++ blog_categories.php
@@ -125,7 +125,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
                             <div class="panel-body">
                             <div class="row">
                                 <div class="col-lg-12">
-                                    <form role="form" method="POST" action="">
+                                    <form role="form" method="POST" action="blog_category.php">
                                        
                                         <div class="form-group">
                                             <label>Category Title</label>
@@ -200,7 +200,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
                                                                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                                                                 <h4 class="modal-title" id="myModalLabel">Edit Category</h4>
                                                             </div>
-                                                            <form role="form" name="frm_edit" method="POST" action="blog_category.php">
+                                                            <form role="form" name="frm_edit" method="POST" action="">
                                                             <div class="modal-body">                                                           
                                        
                                                                     <div class="form-group">
